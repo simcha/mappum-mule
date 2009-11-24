3ebb80c1621bee05d2d0fc993c5871570825a9d0
Jan Topinski <simcha@chatka.org>
2009-11-24 11:37:48 +0100

    Connector based version

--- mule-module-mappum/src/main/java/pl/ivmx/mappum/mule/config/MappumNamespaceHandler.java
+++ mule-module-mappum/src/main/java/pl/ivmx/mappum/mule/config/MappumNamespaceHandler.java
@@ -1,18 +1,25 @@
 package pl.ivmx.mappum.mule.config;
 
+import org.mule.config.spring.handlers.AbstractMuleNamespaceHandler;
 import org.mule.config.spring.parsers.specific.ComponentDefinitionParser;
-import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import org.mule.config.spring.parsers.specific.TransformerDefinitionParser;
+import org.mule.endpoint.URIBuilder;
 
-import pl.ivmx.mappum.mule.component.MappumComponent;
+import pl.ivmx.mappum.mule.MappumComponent;
+import pl.ivmx.mappum.mule.MappumConnector;
+import pl.ivmx.mappum.mule.MappumTransformer;
 
 /**
  * Registers a Bean Definition Parser for handling elements defned in META-INF/mule-mappum.xsd
  *
  */
-public class MappumNamespaceHandler extends NamespaceHandlerSupport
+public class MappumNamespaceHandler extends AbstractMuleNamespaceHandler
 {
     public void init()
     {
-        registerBeanDefinitionParser("component", new ComponentDefinitionParser(MappumComponent.class));
+      registerStandardTransportEndpoints(MappumConnector.MAPPUM, URIBuilder.PATH_ATTRIBUTES);
+      registerBeanDefinitionParser("component", new ComponentDefinitionParser(MappumComponent.class));
+      registerConnectorDefinitionParser(MappumConnector.class);
+      registerBeanDefinitionParser("transformer", new TransformerDefinitionParser(MappumTransformer.class));
     }
 }
